bbf2ffb27b7c0460c3ccb114f55433d4f8ea9b07
WIN_NT\x16g003 <x16g003@chiba-fjb.ac.jp>
2017-12-01 10:08:31 +0900

    no message

--- src/Test002/Test.java
+++ src/Test002/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("ないです");
-        System.out.println("ないアル");
+        System.out.println("なあああああいアル");
         
     }
 
